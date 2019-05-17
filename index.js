ea7d4c30b15a0450a097e896057e7ad61ab4ae22
vinhpham <vinhhvbd@gmail.com>
2019-05-17 21:31:59 +0700

    build file ejs

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express')
 const app = express()
 const port = 5000
 
-app.get('/', (req, res) => res.send('Hello Web-D002!'))
+app.set('view engine','ejs')
+app.get('/api/getlist',(req, res) => {
+    res.json(['a','b','c'])
+})
+app.get('/', (req, res) => {
+    res.render('index', {
+        user: {
+            name: 'Dung'
+        },
+        users: ['apple','samsung','nokia']
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
