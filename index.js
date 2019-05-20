5a1c6673ee67437811149f8f16c6e9f6cca8e8a2
vinhpham <vinhhvbd@gmail.com>
2019-05-20 21:00:48 +0700

    build table

--- index.js
+++ index.js
@@ -2,17 +2,69 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+app.use(express.static(__dirname + '/assets'))
 app.set('view engine','ejs')
 app.get('/api/getlist',(req, res) => {
-    res.json(['a','b','c'])
+    res.json({
+        users: [
+            {
+                name: 'Quyen',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Hanh',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Thuan',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Dung',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Vinh',
+                class: 'PQ-WEB-D002'
+            },
+        ]})
 })
 app.get('/', (req, res) => {
     res.render('index', {
-        user: {
-            name: 'Dung'
-        },
-        users: ['apple','samsung','nokia']
-    })
+        users: [
+            {
+                name: 'Quyen',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Hanh',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Thuan',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Dung',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Vinh',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Tuan',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Hai',
+                class: 'PQ-WEB-D002'
+            },
+            {
+                name: 'Minh',
+                class: 'PQ-WEB-D002'
+            },
+        ]})
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
